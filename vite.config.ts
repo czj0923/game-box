d1f7806b60cf1e219db59d54ef2fabe50058d408
Cao Zhijie <1051029537@qq.com>
2025-08-09 18:21:27 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,31 @@ import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import path from 'node:path';
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, path.resolve(process.cwd(), 'environment'), '');
   return {
     base: env.VITE_BASE_URL,
+    build: {
+      rollupOptions: {
+        output: {
+          // https:///rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : '';
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            );
+          }
+        }
+      }
+    },
     plugins: [
       VueRouter({}),
       vue(),
